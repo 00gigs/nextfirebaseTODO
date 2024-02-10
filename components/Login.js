5f10413873720a21b4ad67a7bc8263e48e624964
00gigs <1onlinegigs@gmail.com>
2024-02-09 23:25:06 -0500

    2

--- components/Login.js
+++ components/Login.js
@@ -4,6 +4,7 @@ export default function Login(){
   return (
    <div className='flex-1 bg-green-300 text-xs sm:text-sm flex-col flex items-center justify-center min-h-screen'>
 <h1 className='font-extrabold font text-2xl sm:text-4xl'>login</h1>
+<div></div>
    </div>
   )
 }
